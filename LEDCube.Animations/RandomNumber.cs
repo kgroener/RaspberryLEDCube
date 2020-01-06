639751fb4c11b5dd3890602e95f2db0d9f438283
Koen Groener <koengroener@outlook.com>
2020-01-06 23:23:48 +0100

    Added more random helper methods

--- LEDCube.Animations/RandomNumber.cs
+++ LEDCube.Animations/RandomNumber.cs
@@ -8,7 +8,7 @@ namespace LEDCube.Animations
 {
     internal static class RandomNumber
     {
-        private static Random _random = new Random();
+        private static readonly Random _random = new Random();
 
         public static int GetRandomNumber(int min, int max)
         {
@@ -18,6 +18,27 @@ namespace LEDCube.Animations
         public static int GetRandomNumber(int max)
         {
             return GetRandomNumber(0, max);
+        }
+
+        public static T GetRandomItem<T>(IEnumerable<T> enumerable)
+        {
+            var enumerableAsArray = enumerable as T[] ?? enumerable.ToArray();
+
+            var length = enumerableAsArray.Length;
+
+            return enumerableAsArray[GetRandomNumber(length - 1)];
+        }
+
+        public static T GetRandomEnumValue<T>() where T : struct, IConvertible
+        { 
+            if (!typeof(T).IsEnum)
+            {
+                throw new InvalidOperationException("Type is not an enum");
+            }
+
+            var values = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+
+            return GetRandomItem(values);
         }
     }
 }
