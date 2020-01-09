ef58e3074b798be52048676d337324e1eefaabe4
Koen Groener <koengroener@outlook.com>
2020-01-09 21:03:36 +0100

    Refactor

--- LEDCube.Animations/Animations/Trigonometry/SineAnimation.cs
+++ LEDCube.Animations/Animations/Trigonometry/SineAnimation.cs
@@ -1,4 +1,4 @@
-﻿using LEDCube.Animations.Animations.Abstracts;
+﻿using LEDCube.Animations.Animations.Trigonometry.Abstracts;
 using LEDCube.Animations.Helpers;
 using System;
 using System.Drawing;
@@ -8,34 +8,20 @@ namespace LEDCube.Animations.Animations.Trigonometry
 {
     public class SineAnimation : TrigonometryAnimation
     {
-        private double _slowDownSpeed;
-        private int _factor;
         private VariableAxis _axis;
+        private int _factor;
+        private double _slowDownSpeed;
 
         public SineAnimation() : base()
         {
             _axis = VariableAxis.YAxis;
         }
 
-        public override TimeSpan PrefferedDuration => TimeSpan.FromSeconds(30);
-
         public override bool AutomaticSchedulingAllowed => true;
-
-        protected override void PrepareInternal()
-        {
-            Speed = RandomNumber.GetRandomNumber(2, 5);
-            _slowDownSpeed = 0;
-            _factor = RandomNumber.GetRandomNumber(2, 4);
-            _axis = RandomNumber.GetRandomEnumValue<VariableAxis>();
-        }
+        public override TimeSpan PrefferedDuration => TimeSpan.FromSeconds(30);
 
         protected override void CleanupInternal()
         {
-        }
-
-        protected override void UpdateInternal(TimeSpan updateInterval)
-        {
-            Speed -= _slowDownSpeed * updateInterval.TotalSeconds;
         }
 
         protected override Color GenerateColor(double x, double y, double z)
@@ -55,14 +41,17 @@ namespace LEDCube.Animations.Animations.Trigonometry
                     a = z.Value;
                     b = y.Value;
                     break;
+
                 case VariableAxis.YAxis:
                     a = x.Value;
                     b = z.Value;
                     break;
+
                 case VariableAxis.ZAxis:
                     a = x.Value;
                     b = y.Value;
                     break;
+
                 default:
                     throw new InvalidOperationException("Invalid axis");
             }
@@ -70,16 +59,27 @@ namespace LEDCube.Animations.Animations.Trigonometry
             return (Math.Sin((_factor * a * b) + IterationValueX) + 1) / 2.0;
         }
 
-        
+        protected override VariableAxis GetVariableAxis()
+        {
+            return _axis;
+        }
+
+        protected override void PrepareInternal()
+        {
+            Speed = RandomNumber.GetRandomNumber(2, 5);
+            _slowDownSpeed = 0;
+            _factor = RandomNumber.GetRandomNumber(2, 4);
+            _axis = RandomNumber.GetRandomEnumValue<VariableAxis>();
+        }
 
         protected override void RequestStopInternal(TimeSpan timeout)
         {
             _slowDownSpeed = Speed / timeout.TotalSeconds;
         }
 
-        protected override VariableAxis GetVariableAxis()
+        protected override void UpdateInternal(TimeSpan updateInterval)
         {
-            return _axis;
+            Speed -= _slowDownSpeed * updateInterval.TotalSeconds;
         }
     }
 }
